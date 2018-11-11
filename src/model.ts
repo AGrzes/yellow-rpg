cd089d875bf3aafbb32bad8ac0c46c03d4b9c20e
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-11-11 22:09:46 +0100

    Basic attribute operation

--- src/model.ts
+++ src/model.ts
@@ -1,9 +1,27 @@
+import * as _ from 'lodash'
+export type EntityId = string
+export type EntityData = any
 export class ModelBuilder {
+  private entities: {[entityId: string]: EntityData} = {}
   public build(): Model {
-    return new Model()
+    return new Model(_.mapValues(this.entities, (entityData) =>
+      new Entity(entityData)))
+  }
+  public addAttributes(id: EntityId, attributes: any) {
+    this.entities[id] = attributes
+  }
+}
+
+export class Entity {
+  constructor(private data) {}
+  public attributes(): any {
+    return this.data
   }
 }
 
 export class Model {
-
+  constructor(private entities: {[entityId: string]: Entity}) {}
+  public get(id: EntityId): Entity {
+    return this.entities[id]
+  }
 }
--- test/model.ts
+++ test/model.ts
@@ -6,5 +6,12 @@ describe('model', function() {
     it('should build model', function() {
       expect(new ModelBuilder().build()).to.be.instanceof(Model)
     })
+
+    it('should add entity attributes', function() {
+      const builder = new ModelBuilder()
+      builder.addAttributes('id', { name: 'value'})
+      const model = builder.build()
+      expect(model.get('id').attributes()).to.be.deep.equals({ name: 'value'})
+    })
   })
 })
